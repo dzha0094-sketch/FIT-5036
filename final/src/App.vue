c8c030ab709e0cbe911105247a95baa2986d1604
Dawen Zhang <dzha0094@student.monash.edu>
2025-10-24 13:07:37 +0800

    update

--- final/src/App.vue
+++ final/src/App.vue
@@ -1,24 +1,22 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import JSONLab from './components/JSONLab.vue'
+import BHeader from './components/BHeader.vue'
+import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <BHeader />
   </header>
 
   <main>
-    <TheWelcome />
+    <LibraryRegistrationForm />
+    <!-- <JSONLab /> -->
   </main>
 </template>
 
 <style scoped>
-header {
+/* header {
   line-height: 1.5;
 }
 
@@ -43,5 +41,5 @@ header {
     place-items: flex-start;
     flex-wrap: wrap;
   }
-}
+} */
 </style>
